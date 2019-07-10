648037f6d4c954f7f946cf63f2185030331c5c95
David McAlpine <dave@mcalpine.io>
2019-07-09 21:31:05 -0400

    fixed offline.html, and updated caching/fetching

--- src/service-worker.js
+++ src/service-worker.js
@@ -3,13 +3,14 @@
 */
 
 // Update when FILES_TO_CACHE changes
-const CACHE_NAME = 'static-cache-v1';
+const CACHE_NAME = 'static-cache-v3';
 
 const FILES_TO_CACHE = [
     './styles.css',
     './TicTacToe.js',
     './icon_192.png',
-    './icon_512.png'
+    './icon_512.png',
+    './offline.html'
 ];
 
 // 1st Step in service worker lifecycle
@@ -54,13 +55,14 @@ self.addEventListener('fetch', (evt) => {
 
     // Prevents browsers default handling of fetch so we can control it ourselves
     evt.respondWith(
-        fetch(evt.request)
-        .catch(() => {
-            return caches.open(CACHE_NAME)
-            .then((cache) => {
-                // return any of the cached assets
-                return cache.match(evt.request.url.split('/').pop());
-            });
+        // Try the cache
+        caches.match(evt.request)
+        .then(function(response) {
+            // Fall back to network
+            return response || fetch(evt.request);
+        }).catch(function() {
+            // If both fail, show a generic fallback:
+            return caches.match('./offline.html');
         })
     );
 });
